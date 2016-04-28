8af536b1a16d7ea2941d068a261674606d30f48a
CJ Cenizal (MacBook Air) <cj@cenizal.com>
2016-04-28 16:20:27 -0700

    Add MVP screenshots and quote.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 
-var isRetina = function() {
-    var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
-            (min--moz-device-pixel-ratio: 1.5),\
-            (-o-min-device-pixel-ratio: 3/2),\
-            (min-resolution: 1.5dppx)";
-    if (window.devicePixelRatio > 1)
-        return true;
-    if (window.matchMedia && window.matchMedia(mediaQuery).matches)
-        return true;
-    return false;
+var isRetina = function isRetina() {
+  if (window.devicePixelRatio > 1) {
+    return true;
+  }
+
+  var mediaQuery =
+    '(-webkit-min-device-pixel-ratio: 1.5),\
+    (min--moz-device-pixel-ratio: 1.5),\
+    (-o-min-device-pixel-ratio: 3/2),\
+    (min-resolution: 1.5dppx)';
+
+  if (window.matchMedia && window.matchMedia(mediaQuery).matches) {
+    return true;
+  }
+
+  return false;
 }();
 
 var images = $('.js-image');
@@ -18,7 +24,8 @@ images.forEach(function(image) {
 
   var lowRes = $image.attr('data-low-res') !== null;
   var suffix = (isRetina && !lowRes) ? '_2x' : '';
-  var src = "./assets/images/" + $image.attr('data-src') + suffix + ".png";
+  var type = '.' + ($image.attr('data-type') || 'png');
+  var src = "./assets/images/" + $image.attr('data-src') + suffix + type;
 
   var $linkedImage = $('<a class="imageContainer"></a>');
   $linkedImage.attr('href', src);
